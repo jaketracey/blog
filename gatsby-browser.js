ec4fbe01e02794fd17a06c39b2e0bc0a8859d225
Jake Tracey <jake.tracey.2.@team.telstra.com>
2020-12-30 13:59:28 +1100

    darkmode starter

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,4 @@ import "fontsource-fanwood-text"
 
 // custom CSS styles
 import "./src/style.scss"
-
-// Highlighting for code blocks
-import "./src/a11y-light.css"
 require("prismjs/plugins/line-numbers/prism-line-numbers.css")
